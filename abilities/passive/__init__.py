b7525cd9eebbebf7353275f2298e9a355f24a23d
Henrique Dias <henriquejndg@gmail.com>
2025-06-12 17:19:34 -0300

    feat: add Tailwind to passive abilities list

--- abilities/passive/__init__.py
+++ abilities/passive/__init__.py
@@ -1,5 +1,6 @@
 from .catnip_spell import CatnipSpell
 from .frozen_claw import FrozenClaw
 from .flaming_paws import FlamingPaws
+from .tailwind import Tailwind
 
-__all__ = ['CatnipSpell', 'FrozenClaw', 'FlamingPaws']
+__all__ = ['CatnipSpell', 'FrozenClaw', 'FlamingPaws', 'Tailwind']
