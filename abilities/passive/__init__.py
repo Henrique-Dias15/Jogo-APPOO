7477bdc6705da85502a314bdd2aea73fc4894195
Henrique Dias <henriquejndg@gmail.com>
2025-06-13 19:43:45 -0300

    feat: add Static Fur to abilities export list

--- abilities/passive/__init__.py
+++ abilities/passive/__init__.py
@@ -5,6 +5,8 @@ from .tailwind import Tailwind
 from .cleaning_tongue import CleaningTongue
 from .pawquake import Pawquake
 from .steel_whiskers import SteelWhiskers
+from .static_fur import StaticFur
 
 __all__ = ['CatnipSpell', 'FrozenClaw', 'FlamingPaws', 
-           'Tailwind', 'CleaningTongue', 'Pawquake', 'SteelWhiskers']
+           'Tailwind', 'CleaningTongue', 'Pawquake', 
+           'SteelWhiskers', 'StaticFur']
