353b9ca4bec5381a88277a88e12beeec04fcd647
Henrique Dias <henriquejndg@gmail.com>
2025-06-13 16:01:16 -0300

    feat: add Pawquake ability with knockback effect on basic attacks

---
+++ abilities/passive/pawquake.py
@@ -0,0 +1,35 @@
+from abilities.base_ability import PassiveAbility
+from utils.settings import *
+
+class Pawquake(PassiveAbility):
+    """Pawquake - Basic attacks have a knockback effect on enemies"""
+    def __init__(self):
+        # Define projectile modifications for knockback effect
+        projectile_mods = {
+            'color': (210, 103, 39),  # Earth color
+            'visual_effect': 'knockback',
+            'particles': 'knockback_trail',  # Placeholder for knockback particles
+            'size': (7, 7)  
+        }
+        
+        super().__init__(
+            name="Pawquake",
+            description="Basic attacks have a chance to knock back enemies",
+            stat_name="",
+            stat_increase=0,  # No stat increase for this ability
+            projectile_modifications=projectile_mods
+        )
+        self.knockback_chance = 1  # 100% chance
+        self.knockback_distance = 50  # Distance to knock back enemies
+    
+    def activate(self, player, **kwargs):
+        super().activate(player, **kwargs)
+        # Add knockback effect to player
+        player.has_pawquake = True
+        player.knockback_chance = self.knockback_chance
+        player.knockback_distance = self.knockback_distance
+        return True
+    
+    def on_upgrade(self):
+        super().on_upgrade()
+        self.knockback_distance += 5  # Increase knockback distance on upgrade
