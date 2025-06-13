49749a2a67ff7de750c1bc232e5f7afb35c19b57
Henrique Dias <henriquejndg@gmail.com>
2025-06-13 14:39:58 -0300

    feat: add Cleaning Tongue to passive abilities list

--- abilities/passive/__init__.py
+++ abilities/passive/__init__.py
@@ -2,5 +2,6 @@ from .catnip_spell import CatnipSpell
 from .frozen_claw import FrozenClaw
 from .flaming_paws import FlamingPaws
 from .tailwind import Tailwind
+from .cleaning_tongue import CleaningTongue
 
-__all__ = ['CatnipSpell', 'FrozenClaw', 'FlamingPaws', 'Tailwind']
+__all__ = ['CatnipSpell', 'FrozenClaw', 'FlamingPaws', 'Tailwind', 'CleaningTongue']
