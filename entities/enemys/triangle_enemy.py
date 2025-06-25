706fc901a73161fa4a94657669f9f5b8694b7f7f
Lucaszioli <lucas.zioli.04@gmail.com>
2025-06-25 00:19:13 -0300

    feat: adiciona spriter do shooter inicial

--- entities/enemys/triangle_enemy.py
+++ entities/enemys/triangle_enemy.py
@@ -14,21 +14,19 @@ class TriangleEnemy(BaseShooter):
             y=y,
             screen_width=screen_width,
             screen_height=screen_height,
-            size=(25, 25),  # Medium size
+            size=(125, 125),  # Medium size
             color=BLUE,   # Blue color
             speed=speed,        # Fast speed
             hp=hp,
             damage=damage,
             projectile_cooldown=projectile_cooldown,
-            projectile_damage=projectile_damage
+            projectile_damage=projectile_damage,
+            spritesheet='assets/images/bottle_spray/Pshpsh.png',
+            frame_ammount=2,  # Number of frames in the sprite sheet
+            frame_delay=200,
         )
         self.pos_x = float(self.rect.x)
         self.pos_y = float(self.rect.y)
-        
-        # Create a triangle shape
-        self.original_image = pygame.Surface((25, 25), pygame.SRCALPHA)
-        pygame.draw.polygon(self.original_image, BLUE, [(12, 0), (0, 25), (25, 25)])
-        self.image = self.original_image
 
     def update(self, *args, **kwargs):
         """Move enemy towards player and shoot projectiles"""
@@ -41,17 +39,22 @@ class TriangleEnemy(BaseShooter):
         if distance != 0:
             dx, dy = dx / distance, dy / distance
 
+        self.angle = math.degrees(math.atan2(-dy, dx))  # Adjust angle for pygame coordinate system
+
         # Calculate distance to player
         if distance > 500: # If the player is far away, move towards them
             self.pos_x += dx * self.speed
             self.pos_y += dy * self.speed
             self.shooter = False
+            self.has_animation = False
         elif distance < 300: # If the player is too close, move away from them
             self.pos_x -= dx * self.speed
             self.pos_y -= dy * self.speed
             self.shooter = False
+            self.has_animation = False
         else:  # If the player is at a medium distance, shoot projectiles
             self.shooter = True
+            self.has_animation = True
 
         self.pos_x = max(0, min(self.pos_x, self.screen_width - self.rect.width))
         self.pos_y = max(0, min(self.pos_y, self.screen_height - self.rect.height))
@@ -59,6 +62,9 @@ class TriangleEnemy(BaseShooter):
         # Atualize o rect a partir da posição real
         self.rect.x = int(self.pos_x)
         self.rect.y = int(self.pos_y)
+
+        self.update_animation_turning()
+
     def kill(self):
         """Handle enemy death, drop experience, and remove from groups"""
         return super().kill(20)
